ac840d8cebf3bdcd914e6e66ec339b6fabf15ecb
Sanghyun Park <shpaddress23@gmail.com>
2026-06-25 22:32:12 +0900

    fix: enable email_on_failure alert in Airflow DAG default_args

--- dags/dbt_incremental_pipeline.py
+++ dags/dbt_incremental_pipeline.py
@@ -35,7 +35,7 @@ default_args = {
     "owner": "airflow",
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
-    "email_on_failure": False,
+    "email_on_failure": True,
 }
 
 with DAG(
