031fb218f6526b61567e106f06d4df3581acdb6c
Sanghyun Park <shpaddress23@gmail.com>
2026-07-01 10:00:00 +0900

    fix: wrap range() args to pass SQLFluff line-length check

--- models/utilities/metricflow_time_spine.sql
+++ models/utilities/metricflow_time_spine.sql
@@ -6,7 +6,11 @@
 
 with days as (
     select range::date as date_day
-    from range(date '2019-01-01', date_trunc('year', current_date) + interval '3 years', interval '1 day')
+    from range(
+        date '2019-01-01',
+        date_trunc('year', current_date) + interval '3 years',
+        interval '1 day'
+    )
 )
 
 select date_day from days
